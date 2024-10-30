e664bb149333b8fd6b47118a8cd3c8437f10d5bc
Amayuru1999 <amarasingheau@gmail.com>
2024-10-30 11:29:11 +0530

    feat: Added deploy.ts with the functionality #1

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,21 @@
-import {ethers} from "hardhat";
+import { ethers } from "hardhat";
 
-async function main(){
-    const Chai = await ethers.getContractFactory("Chai");
+async function main() {
+    // Fetch the ContractFactory for the "chai" contract
+    const Chai = await ethers.getContractFactory("chai");
+
+    // Deploy the contract
     const chai = await Chai.deploy();
+
+    // Ensure the contract is properly deployed
     await chai.deployed();
+
+    // Log the address where the contract is deployed
     console.log("Chai deployed to:", chai.address);
 }
 
+// Handle errors
 main().catch((error) => {
-    console.log(error);
+    console.error(error);
     process.exitCode = 1;
 });
