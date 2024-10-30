caa326b19ea409c294bb524020127f6877bd77c1
Amayuru1999 <amarasingheau@gmail.com>
2024-10-30 14:16:42 +0530

    feat: Added client vite project #1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from "react";
+import { ethers } from "hardhat";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [state, setState] = useState({
+        provider: null,
+        signer: null,
+        contract: null,
+    })
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const template = async () => {
+            const contractAddress = "";
+            const contractABI = "";
+            //MetaMask part
+            //1. In order to do transactions on goerli testnet
+            //2. MetaMask consist of infura api which actually help in connecting to the blockchain
+            const { ethereum } = window;
+
+            const account = await ethereum.request({method: 'eth_requestAccounts'});
+
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+
+            const contract = new ethers.Contract(
+                contractAddress,
+                contractABI,
+                signer
+            )
+        }
+        template();
+    }, []);
+
+    return (
+        <>
+
+        </>
+    )
 }
 
 export default App
