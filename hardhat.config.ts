1e112c856761ddc2f3eb13c3363ff7a8cf298ada
Amayuru1999 <amarasingheau@gmail.com>
2024-11-07 13:33:57 +0530

    chore: Changed into Ethereum Mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
+    mainnet: {
       url: GOERLI_URL,
       accounts: [PRIVATE_KEY],
     },
